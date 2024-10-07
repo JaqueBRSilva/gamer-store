1c9aa62c21970f6ba525f4ff798abc10923e3b35
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-10-06 21:17:45 -0300

    feat: criação de `backend`

---
+++ gam3r.store/apps/backend/src/db/db.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaProvider } from './prisma.provider';
+
+
+@Module({
+    providers: [PrismaProvider],
+    exports: [PrismaProvider],
+})
+
+export class DbModule {}
